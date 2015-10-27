ba3a363e6fb4c7731cbe794dd3b6908ddd44f03d
RGIB <gibellini.roberto@gmail.com>
2015-10-27 11:05:34 +0100

    Kona-common : removed BOARD_SEPOLICY_UNION
    Change-Id: Ieffe072d463e512c1b3c895767974c9fab5f7571

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,29 +41,4 @@ BOARD_BATTERY_DEVICE_NAME := "battery"
 -include vendor/samsung/kona/BoardConfigVendor.mk
 
 # Selinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/kona-common/selinux
-
-BOARD_SEPOLICY_UNION += \
-    bluetooth.te \
-    file_contexts \
-    te_macros \
-    device.te \
-    dhcp.te \
-    domain.te \
-    file.te \
-    init.te \
-    kickstart.te \
-    mediaserver.te \
-    netd.te \
-    netmgrd.te \
-    qmux.te \
-    rild.te \
-    secril.te \
-    shell.te \
-    sysinit.te \
-    system.te \
-    ueventd.te \
-    untrusted_app.te \
-    wpa_supplicant.te
-
+BOARD_SEPOLICY_DIRS := device/samsung/kona-common/selinux
