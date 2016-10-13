acdcb496229040777cf59d8a44a66360b3295a32
RGIB <gibellini.roberto@gmail.com>
2016-10-13 13:31:22 +0200

    kona-common : wifi cleanup
    Change-Id: Id54f80238d56b7120f6047e5bdd0c0f811b26cb8

--- kona-common.mk
+++ kona-common.mk
@@ -36,15 +36,9 @@ PRODUCT_PACKAGES += \
     tiny_hw \
     tinyplay \
     SamsungServiceMode \
-    VoicePlus \
     Stk
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
-    $(LOCAL_PATH)/configs/nvram_mfg.txt_murata:system/etc/wifi/nvram_mfg.txt_murata \
-    $(LOCAL_PATH)/configs/nvram_net.txt:system/etc/wifi/nvram_net.txt \
-    $(LOCAL_PATH)/configs/nvram_net.txt_murata:system/etc/wifi/nvram_net.txt_murata \
-    $(LOCAL_PATH)/configs/80mac:system/etc/init.d/80mac \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # Bluetooth
