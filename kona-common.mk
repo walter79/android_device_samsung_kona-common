37f9f132ca1fc954cca30b16c4686ec94eb196d0
RGIB <gibellini.roberto@gmail.com>
2016-12-22 01:04:33 +0100

    kona-common : add ueventd to the build
    Change-Id: I723b8fc5c5fc3b16f35b6b561476843e708e6db1

--- kona-common.mk
+++ kona-common.mk
@@ -31,7 +31,9 @@ TARGET_HAS_CAM_FLASH := false
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
-    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
+    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc
 
 # Stock MM blobs
 PRODUCT_PACKAGES += \
