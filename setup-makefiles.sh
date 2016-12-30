e96ade493a09825361bf404fb5f65be3f7e5fead
Simon Shields <keepcalm444@gmail.com>
2016-12-31 01:40:45 +1100

    kona: switch to generic extract utils
    Change-Id: Ieaef5e0de277141d96786a3d5a60bda4cf0bbe2d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -33,33 +33,24 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
-
 # Initialize the helper for common device
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
+# Copyright headers and common guards
+write_headers "n5100 n5110 n5120"
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+write_footers
 
 # Reinitialize the helper for the device-specific blobs
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC"
+# Copyright headers and guards
+write_headers
 
-"$MY_DIR"/setup-makefiles.sh
+# The device-specific blobs
+write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt
 
-export DEVICE=nodevice
-./../smdk4412-common/extract-files.sh $@
+# Done
+write_footers
